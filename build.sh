b2b5b950bdd86c0c93792582e65deadb14dd3c33
Yanagi-1112 <yanagichan2000@gmail.com>
2026-07-18 03:04:10 +0900

    画面だけオフ機能を追加（手動＋フタ閉じ自動）、caffeinate -d を除去、v1.1

--- build.sh
+++ build.sh
@@ -22,8 +22,8 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
   <key>CFBundleIdentifier</key><string>com.yanagi.fumin</string>
   <key>CFBundleExecutable</key><string>Fumin</string>
   <key>CFBundlePackageType</key><string>APPL</string>
-  <key>CFBundleShortVersionString</key><string>1.0</string>
-  <key>CFBundleVersion</key><string>1</string>
+  <key>CFBundleShortVersionString</key><string>1.1</string>
+  <key>CFBundleVersion</key><string>2</string>
   <key>LSMinimumSystemVersion</key><string>13.0</string>
   <key>LSUIElement</key><true/>
   <key>NSPrincipalClass</key><string>NSApplication</string>
@@ -34,7 +34,7 @@ PLIST
 
 echo "▶ Swift コンパイル"
 swiftc -O -swift-version 5 \
-  -framework Cocoa -framework ServiceManagement \
+  -framework Cocoa -framework ServiceManagement -framework IOKit \
   -o "$MACOS/Fumin" "$SRC_DIR/Fumin.swift"
 
 echo "▶ アドホック署名（ログイン項目登録に必要）"
